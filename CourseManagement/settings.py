a10f411080d824b10b3ecdf1356bdcfb25571206
Yasmine Sidi Mansour <yasmine.sidimansour@univ-constantine2.dz>
2025-11-25 22:10:00 +0100

    save1

--- CourseManagement/settings.py
+++ CourseManagement/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'rest_framework',           # DRF
+    'django_prometheus',
 ]
 
 MIDDLEWARE = [
