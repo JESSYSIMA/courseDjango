e7dbbe04dc5507eeb42e36d2087c7cd95ec923b6
Yasmine Sidi Mansour <yasmine.sidimansour@univ-constantine2.dz>
2025-12-08 21:25:03 +0100

    last

--- courses/apps.py
+++ courses/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 from threading import Thread
-from .consumers.student_events import start_consumer
 
-class CourseConfig(AppConfig):
-    name = 'course'
 
+class CoursesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'courses'
+    
+    
     def ready(self):
-        Thread(target=start_consumer).start()
+        from .consumers.student_events import start_consumer
+        # Lancer le consumer dans un thread pour ne pas bloquer le démarrage de Django
+        Thread(target=start_consumer, daemon=True).start()
